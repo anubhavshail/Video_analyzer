acd2d0ff6a215e2d7563d8bcc9fae2cf6b6549c6
anubhavshail <anubhavshail12@gmail.com>
2022-10-22 19:51:50 +0530

    main file modified

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-for flask import Flask, render_template, response
+from flask import Flask, render_template, response
 
 app = Flask(__name__)
 
 @app.route('/')
-def index:
+def index():
     return render_template('intex.html')
+
+@app.route('/FallDetection', methods=['GET', 'POST'])
+def falldetection():
+    return render_template('FallDetection.html')
+
+@app.route('/ObjectDetection', methods=['GET', 'POST'])
+def objectdection():
+    return render_template('ObjectDetection.html')
+
+@app.route('/CarCrashDetection', methods=['GET', 'POST'])
+def carcrashdetection():
+    return render_template('CarCrashDetection.html')
+
+@app.route('/SocialDistancingDetection', methods=['GET', 'POST'])
+def carcrashdetection():
+    return render_template('SocialDistancingDetection.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
 
